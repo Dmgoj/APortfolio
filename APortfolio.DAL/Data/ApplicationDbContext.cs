3d9b15856070960fbc727b02432533b564764933
Dmgoj <domagoj.grgic@outlook.com>
2024-09-16 08:16:46 +0200

    feat(db): add DbSet for Entities

--- APortfolio.DAL/Data/ApplicationDbContext.cs
+++ APortfolio.DAL/Data/ApplicationDbContext.cs
@@ -7,9 +7,16 @@ namespace APortfolio.DAL.Data
 {
     public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
+        public DbSet<AppUser> AppUsers{ get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Project> Projects{ get; set; }
+        public DbSet<Media> Medias { get; set; }
+
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            
         }
     }
 }
