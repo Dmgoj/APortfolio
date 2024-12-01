cfaf1a281a992eed5b73f13d82ca63d5e2719f12
Dmgoj <domagoj.grgic@outlook.com>
2024-12-01 19:13:30 +0100

    feat: inject IPortfolioService into HomeController

--- APortfolio.Web/Controllers/HomeController.cs
+++ APortfolio.Web/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using APortfolio.BLL.Services;
 using APortfolio.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,21 +8,21 @@ namespace APortfolio.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IPortfolioService _service;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IPortfolioService portfolioService)
         {
             _logger = logger;
+            _service = portfolioService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var portfolios = await _service.GetAllAsync();
+            return View(portfolios);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+      
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
