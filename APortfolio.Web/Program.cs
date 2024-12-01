a6977cc522ff0995415dbe4770b7c86e6f679ee1
Dmgoj <domagoj.grgic@outlook.com>
2024-12-01 19:19:02 +0100

    chore: configure application services and dependency injection

--- APortfolio.Web/Program.cs
+++ APortfolio.Web/Program.cs
@@ -1,5 +1,7 @@
+using APortfolio.BLL.Services;
 using APortfolio.DAL.Data;
 using APortfolio.DAL.Entities;
+using APortfolio.DAL.Repositories;
 using APortfolio.Web.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
@@ -21,10 +23,20 @@ namespace APortfolio.Web
             builder.Services.AddIdentity<AppUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false) //TODO change to true later
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Identity/Account/Login"; 
+                options.AccessDeniedPath = "/Account/AccessDenied";
+            });
 
             // In Program.cs
             builder.Services.AddTransient<IEmailSender, EmailSender>();
 
+            builder.Services.AddScoped<IPortfolioService, PortfolioService>();
+            builder.Services.AddScoped<IPortfolioRepository, PortfolioRepository>();
+
+            builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+            builder.Services.AddScoped<IProjectService, ProjectService>();
 
 
             builder.Services.AddRazorPages();
