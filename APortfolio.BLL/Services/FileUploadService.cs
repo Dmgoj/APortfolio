7130b54ee07a2d9568d0086585c5b4225c0d60f6
Dmgoj <domagoj.grgic@outlook.com>
2024-12-08 23:28:11 +0100

    feat(service): add FileUploadService

---
+++ APortfolio.BLL/Services/FileUploadService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace APortfolio.BLL.Services
+{
+    public class FileUploadService : IFileUploadService
+    {
+        private readonly string _imagePath;
+
+        public FileUploadService(IConfiguration configuration)
+        {
+            _imagePath = configuration["ImageStoragePath"];
+        }
+
+        public async Task<string> UploadImageAsync(IFormFile image)
+        {
+            if (image == null || image.Length == 0)
+            {
+                return "/images/projects/default.jpeg";
+            }
+
+            // Generate filename with a timestamp
+            var timestamp = DateTime.Now.ToString("ddMMyyyy_HHmmss");
+            var fileName = $"{timestamp}_{Path.GetFileName(image.FileName)}";
+            var filePath = Path.Combine("wwwroot", _imagePath, fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await image.CopyToAsync(stream);
+            }
+
+            return Path.Combine("/",_imagePath, fileName).Replace("\\", "/"); ;
+        }
+
+        public Task<string> UploadVideoAsync(IFormFile videoFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
